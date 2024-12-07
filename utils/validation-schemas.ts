33c37adcd024a6354dca846fd220cec1dbe6d7a9
masterodi <ovddrg@gmail.com>
2024-12-07 22:23:17 +0200

    chore: Update validation

--- utils/validation-schemas.ts
+++ utils/validation-schemas.ts
@@ -1,19 +1,17 @@
 import { z } from 'zod'
 
+const RequiredMessage = (field: string) => `${field} is required`
+const MinLengthMessage = (field: string, length: number) => `${field} must have at least ${length} characters`
+
 export const RegisterPayloadSchema = z.object({
-  username: z.string(),
-  password: z.string(),
+  username: z.string().trim().min(3, MinLengthMessage('Username', 3)),
+  password: z.string().trim().min(8, MinLengthMessage('Password', 8)),
 })
 
 export const RegisterFormSchema = z.object({
-  username: z.string(),
-  password: z.string(),
-  passwordConfirm: z.string(),
-})
-
-export const LoginFormSchema = z.object({
-  username: z.string(),
-  password: z.string(),
+  username: z.string().trim().min(3, MinLengthMessage('Username', 3)),
+  password: z.string().trim().min(8, MinLengthMessage('Password', 8)),
+  passwordConfirm: z.string().trim().min(8, MinLengthMessage('Password', 8)),
 })
 
 export const LoginPayloadSchema = z.object({
@@ -21,8 +19,13 @@ export const LoginPayloadSchema = z.object({
   password: z.string(),
 })
 
+export const LoginFormSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+})
+
 export const DeskCreatePayloadSchema = z.object({
-  label: z.string().trim().min(1, 'Label is required'),
+  label: z.string().trim().min(1, RequiredMessage('Desk Label')),
 })
 
 export const DesksCreatePayloadSchema = z.union([
@@ -33,9 +36,9 @@ export const DesksCreatePayloadSchema = z.union([
 export const DeskUpdatePayloadSchema = DeskCreatePayloadSchema.partial()
 
 export const DesksQueryParamsSchema = z.object({
-  start: z.string().date().optional(),
-  end: z.string().date().optional(),
-})
+  start: z.string().date(),
+  end: z.string().date(),
+}).partial()
 
 export const BookingCreatePayloadSchema = z.object({
   deskId: z.string(),
