ee5b5f3c5cde69cb1e819177dabbc574ff94fcb1
masterodi <ovddrg@gmail.com>
2024-12-14 01:14:22 +0200

    feat: Add Calendar

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,38 @@
 <script setup lang="ts">
+import { getLocalTimeZone, today } from '@internationalized/date'
+import { z } from 'zod'
+
 definePageMeta({
   middleware: ['auth-guard-middleware'],
 })
 
-const { user } = useUserSession()
+const schema = z.object({
+  date: z.any(),
+})
+
+const state = reactive({
+  date: today(getLocalTimeZone()),
+})
 </script>
 
 <template>
   <div>
-    <div>{{ JSON.stringify(user) }}</div>
-    <div class="w-4xl" />
+    <div class="flex justify-center">
+      <UForm
+        :schema="schema"
+        :state="state"
+      >
+        <UFormField
+          label="Pick a date"
+          name="date"
+        >
+          <UCalendar />
+        </UFormField>
+
+        <UButton type="submit">
+          Book desk
+        </UButton>
+      </UForm>
+    </div>
   </div>
 </template>
